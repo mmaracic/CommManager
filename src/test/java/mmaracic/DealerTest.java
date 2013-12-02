c3c8702693a753061d00f6177df0be5634d21dff
Marijo Maračić <marijo.maracic@gmail.com>
2013-12-02 13:52:33 +0100

    Final beta of the CommManager tests. Liveness tests can fail, not a big teal the way they're currently set up. Other test must not fail if the code is to work correctly.

--- src/test/java/mmaracic/DealerTest.java
+++ src/test/java/mmaracic/DealerTest.java
@@ -24,6 +24,8 @@ import static org.junit.Assert.*;
  *
  * @author Marijo
  * Tests router dealer pattern. Service acts as router-dealer.
+ * Tests check whether all the subscribers (reqs) got their messages and whether they are all live (served equally often)
+ * Liveness is likely to fail if threads are not synchronized; it is not a big problem
  */
 public class DealerTest
 {
@@ -34,11 +36,16 @@ public class DealerTest
 
     private static final Random random = new Random(System.currentTimeMillis());
     private static final HashMap<Integer, byte[]> requestOwner = new HashMap<>();
+    private static final HashMap<Integer, Integer> requestID = new HashMap<>();
     
     private static final int noEntities = 10;
 
     private static final HashMap<Integer, Messages.Entity> entitiesReq1 = new HashMap<>();
     private static final HashMap<Integer, Messages.Entity> entitiesReq2 = new HashMap<>();
+    
+    private static int liveState = -1;
+    private static boolean liveCheck = true;
+ 
 
     public DealerTest()
     {
@@ -64,7 +71,8 @@ public class DealerTest
                                         catch (Exception ex) { }
                                         Messages.ReqEntity req_entity_message = (Messages.ReqEntity) message;
                                         requestOwner.put(req_entity_message.getId(), peer_id);
-                                        System.out.println("Forwarding request for "+req_entity_message.getId());
+                                        requestID.put(req_entity_message.getId(), request_id);
+                                        //System.out.println("Forwarding request for "+req_entity_message.getId());
                                         service.request("tcp://*:5559", req_entity_message, Messages.AddEntity.getDescriptor(), new MessageHandler() { 
                                             public void handleMessage(GeneratedMessage message, byte[] peer_id, int request_id)
                                             {
@@ -75,8 +83,9 @@ public class DealerTest
                                                     catch (Exception ex) { }
                                                     Messages.AddEntity add_entity_message = (Messages.AddEntity) message;
                                                     byte[] peer_id_orig = requestOwner.get(add_entity_message.getEntity().getId());
-                                                    service.reply(add_entity_message, peer_id_orig, request_id);
-                                                    System.out.println("Forwarding response for "+add_entity_message.getEntity().getId());
+                                                    int request_id_orig = requestID.get(add_entity_message.getEntity().getId());
+                                                    service.reply(add_entity_message, peer_id_orig, request_id_orig);
+                                                    //System.out.println("Forwarding response for "+add_entity_message.getEntity().getId());
                                              }
                                         });
                                  }
@@ -99,7 +108,7 @@ public class DealerTest
                                                                 .build())
                                                                 .build();
                                         rep.reply(add_entity_message, peer_id, request_id);
-                                        System.out.println("Replying with "+add_entity_message.getEntity().getId());
+                                        //System.out.println("Replying with "+add_entity_message.getEntity().getId());
                                 }
                             });
         Thread req1Thread = new Thread(req1);
@@ -127,9 +136,13 @@ public class DealerTest
                                         Thread.sleep(250);
                                 }
                                 catch (Exception ex) { }
+                                if (liveState==0)
+                                    liveCheck = false;
+                                else
+                                    liveState=0;
                                 Messages.AddEntity add_entity_message = (Messages.AddEntity) message;
                                 entitiesReq1.put(request_id, add_entity_message.getEntity());
-                                System.out.println("Received entity "+add_entity_message.getEntity().getId());
+                                //System.out.println("Req1 received entity "+add_entity_message.getEntity().getId()+" request_id: "+request_id);
                         }
                 });
             }
@@ -146,8 +159,13 @@ public class DealerTest
                                         Thread.sleep(250);
                                 }
                                 catch (Exception ex) { }
+                                if (liveState==1)
+                                    liveCheck = false;
+                                else
+                                    liveState=1;
                                 Messages.AddEntity add_entity_message = (Messages.AddEntity) message;
                                 entitiesReq2.put(request_id, add_entity_message.getEntity());
+                                //System.out.println("Req2 received entity "+add_entity_message.getEntity().getId()+" request_id: "+request_id);
                         }
                 });                
             }   
@@ -188,13 +206,42 @@ public class DealerTest
     @Test
     public void countResponsesReq1()
     {
-        assertTrue("Req1 didnt get all the responses: "+entitiesReq1.size()+" of "+noEntities/2,entitiesReq1.size()==noEntities/2);
+        //even entities
+        boolean check = true;
+        for (int i=0; i<noEntities/2;i++)
+        {
+            Messages.Entity e = entitiesReq1.get(i+1);
+            if (e.getId() != i*2)
+            {
+                check = false;
+                break;
+            }
+        }
+        assertTrue("Req1 didnt get all the right entities: "+entitiesReq1.size()+" of "+noEntities/2,check);
     }
     
     //Count responses for req2
     @Test
     public void countResponsesReq2()
     {
-        assertTrue("Req2 didnt get all the responses: "+entitiesReq2.size()+" of "+noEntities/2,entitiesReq2.size()==noEntities/2);
+        //odd entities
+        boolean check = true;
+        for (int i=0; i<noEntities/2;i++)
+        {
+            Messages.Entity e = entitiesReq2.get(i+1);
+            if (e.getId() != i*2+1)
+            {
+                check = false;
+                break;
+            }
+        }
+        assertTrue("Req2 didnt get all the right entities: "+entitiesReq2.size()+" of "+noEntities/2,check);
     }
+
+    //testing liveness
+    @Test
+    public void testLiveness()
+    {
+        assertTrue("Clients are not served equally often.",liveCheck);        
+    }    
 }
--- src/test/java/mmaracic/PublisherTest.java
+++ src/test/java/mmaracic/PublisherTest.java
@@ -24,8 +24,10 @@ import static org.junit.Assert.*;
  *
  * @author Marijo
  * Service is a publisher that publishes entities to the subscribers
- * Test checks whether all the subscribers got their messages
- * Publisher cant have own process thread
+ * Tests check whether all the subscribers got their messages and whether they are all live (served equally often)
+ * Liveness is likely to fail if threads are not synchronized; it is not a big problem
+ * Publisher cant have own process thread due to lack of thread safety in zeromq socket
+ * We need to allow the subscribers to setup properly because publisher will drop messages it cannot deliver
  */
 public class PublisherTest
 {
@@ -38,6 +40,9 @@ public class PublisherTest
     private static final HashMap<Integer, Messages.Entity> clientEntities2 = new HashMap<>();
     
     private static final int noEntities = 10;
+    
+    private static int liveState = -1;
+    private static boolean liveCheck = true;
     
     public PublisherTest()
     {
@@ -63,6 +68,10 @@ public class PublisherTest
                                                 Thread.sleep(250);
                                         }
                                         catch (Exception ex) { }
+                                        if (liveState==0)
+                                            liveCheck = false;
+                                        else
+                                            liveState=0;
 
                                         Messages.AddEntity add_entity_message = (Messages.AddEntity) message;
                                         Messages.Entity entity = add_entity_message.getEntity();
@@ -80,6 +89,10 @@ public class PublisherTest
                                                 Thread.sleep(250);
                                         }
                                         catch (Exception ex) { }
+                                        if (liveState==1)
+                                            liveCheck = false;
+                                        else
+                                            liveState=1;
 
                                         Messages.AddEntity add_entity_message = (Messages.AddEntity) message;
                                         Messages.Entity entity = add_entity_message.getEntity();
@@ -92,6 +105,9 @@ public class PublisherTest
 
         Thread clientThread2 = new Thread(client2);
         clientThread2.start();
+        
+        //Allow the subscribers to setup because publisher will drop messages it cannot deliver
+        Thread.sleep(1000);
 
 //        Thread serviceThread = new Thread(service);
 //        serviceThread.start();
@@ -143,13 +159,39 @@ public class PublisherTest
     @Test
     public void testFirstClient()
     {
-        assertTrue("Client1 did not receive all the entities: "+clientEntities1.size()+" of "+noEntities,clientEntities1.size()==noEntities);
+        boolean check = true;
+        for(int i=0; i<noEntities; i++)
+        {
+            if (!clientEntities1.containsKey(i))
+            {
+                check = false;
+                break;
+            }
+        }
+        assertTrue("Client1 did not receive all the entities: "+clientEntities1.size()+" of "+noEntities,check);
     }
 
     //Testing second client
     @Test
     public void testSecondClient()
     {
-        assertTrue("Client2 did not receive all the entities: "+clientEntities2.size()+" of "+noEntities,clientEntities2.size()==noEntities);
+       boolean check = true;
+        for(int i=0; i<noEntities; i++)
+        {
+            if (!clientEntities2.containsKey(i))
+            {
+                check = false;
+                break;
+            }
+        }
+        assertTrue("Client2 did not receive all the entities: "+clientEntities2.size()+" of "+noEntities,check);
     }
+    
+    //testing liveness
+    @Test
+    public void testLiveness()
+    {
+        assertTrue("Clients are not served equally often.",liveCheck);        
+    }
+    
 }
